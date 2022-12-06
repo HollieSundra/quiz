baf6628745761c7484192b8c2e539de15edbccf8
Nerdy Birdy <holliesundra@gmail.com>
2022-12-06 01:26:44 -0700

    final push

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # quiz
 Timed quiz with score at the end.
+
+
+#Description
+
+Time quiz that keeps score and stores it to the local storage.
+
+
+#Usage
+
+Play the quiz game and challenge you highscore.
+
+#Credits
+
+https://www.youtube.com/watch?v=WUBhpSRS_fk
+
+https://www.youtube.com/watch?v=DFhmNLKwwGw&t=16s
+
+https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById#:~:text=See%20also-,Document.getElementById(),to%20a%20specific%20element%20quickly.
+
+https://www.w3schools.com/howto/howto_js_media_queries.asp
+
+https://www.w3schools.com/js/js_timing.asp
+
+https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+
+
+License
+None
+
---
+++ js/questions.js
@@ -0,0 +1,54 @@
+//Create an array for questions
+
+let questions = [
+    {
+        numb: 1,
+        question: "What is the world's smallest fox breed?",
+        answer: "Fennic",
+        options: [
+           "Fennic",
+           "Artic",
+           "Red Fox" 
+        ]
+    },
+    {
+        numb: 2,
+        question: "Where is the only place on Earth that foxes can not be found?",
+        answer: "Antartica",
+        options: [
+           "Iceland",
+           "Antartica",
+           "Brazil" 
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is a baby fox called?",
+        answer: "Kit",
+        options: [
+           "Pup",
+           "Kit",
+           "Cub" 
+        ]
+    },
+    {
+        numb: 4,
+        question: "What do foxes walk on?",
+        answer: "Toes",
+        options: [
+           "Feet",
+           "Hands",
+           "Toes" 
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is a female fox called?",
+        answer: "Vixan",
+        options: [
+           "Vixan",
+           "Femme",
+           "Lady" 
+        ]
+    }
+];
